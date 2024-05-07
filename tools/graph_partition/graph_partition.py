39981ccbce6a397d1457f4f30d15e6c755ea20cd
liangrenhao <francking@189.cn>
2024-05-07 01:34:09 +0000

    update tools/graph_partition/graph_partition.py.
    Signed-off-by: liangrenhao <francking@189.cn>

--- tools/graph_partition/graph_partition.py
+++ tools/graph_partition/graph_partition.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.contrlib import graph_editor as ge
+from tensorflow.contrib import graph_editor as ge
 class GraphPartitioner:
     def __init__(self):
         self.signature_def = None
@@ -10,7 +10,7 @@ class GraphPartitioner:
         self.tensor_node_lookup = dict()
         self.heavy_load_ops = []
         self.embedding_lookup_op_type = None
-        self.first_heavy_load_on_spares_path = set()
+        self.first_heavy_load_on_sparse_path = set()
         self.first_op_after_lookup = []
         self.seen = set()
         self.post_out = set()
@@ -21,7 +21,7 @@ class GraphPartitioner:
         self.input_nodes = []
         self.output_nodes = []
 
-    def set_emedding_lookup_op_type(self,s):
+    def set_embedding_lookup_op_type(self,s):
         self.embedding_lookup_op_type = s
 
     def has_gary_downstreams(self, op):
@@ -37,15 +37,15 @@ class GraphPartitioner:
         in_str = set()
         out_str = set()
         inputs_set = set()
-        outputs = set()
-        for op in self.graph.get_oprations():
+        outputs_set = set()
+        for op in self.graph.get_operations():
             if self._is_embedding_lookup(op):
                 self.sparse_lookup_ops.append(op)
         if not self.sparse_lookup_ops:
             print("aaaaaaaaaaaaaaaaaa")
-            for op in self.graph.get_oprations():
+            for op in self.graph.get_operations():
                 is_top_op = True
-                for op1 in self.graph.get_oprations():
+                for op1 in self.graph.get_operations():
                     for tensor in op1.outputs:
                         if tensor in op.inputs:
                             is_top_op = False
@@ -62,18 +62,18 @@ class GraphPartitioner:
                 self.sparse_lookup_tensors.extend(op.outputs)
 
         print(self.sparse_lookup_ops)
-        for op in self.graph.get_oprations():
+        for op in self.graph.get_operations():
             for tensor in self.sparse_lookup_tensors:
                 if tensor in op.inputs:
                     self.input_nodes.append(op)
-        for k,v in self.signature_def.output.items():
+        for k,v in self.signature_def.outputs.items():
             op_name = str(v).split("\n")[0].replace(' ', "").replace('"',"").split(":")[1].split(":")[0]
-            for op in self.graph.get_oprations();
-            if op.name == op_name:
-                self.output_nodes.append(op)
+            for op in self.graph.get_operations():
+                if op.name == op_name:
+                    self.output_nodes.append(op)
 
         floatUps = []
-        to_expend = []
+        to_expand = []
         in_str = []
 
         for op in self.input_nodes:
@@ -81,21 +81,22 @@ class GraphPartitioner:
                 if op.name not in in_str:
                     in_str.append(op.name)
             else:
-                to_expend.append(op)
+                to_expand.append(op)
 
-        while to_expend:
+        while to_expand:
             candidates = []
-            for top in to_expend:
-                for op in self.graph.get_oprations():
+            for top in to_expand:
+                for op in self.graph.get_operations():
                     for tensor in op.inputs:
                         if tensor in top.outputs:
                             candidates.append(op)
-            to_expend = []
+            to_expand = []
             for op in candidates:
                 if op.type not in floatUps:
-                    in_str.append(op.name)
+                    if op.name not in in_str:
+                        in_str.append(op.name)
                 else:
-                    to_expend.append(op)
+                    to_expand.append(op)
         return str(in_str), str([op.name for op in self.output_nodes])
 
     def _is_embedding_lookup(self, op):
