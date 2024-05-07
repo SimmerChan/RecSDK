541b2c0e57dfc6f47a8f7cfc1584b86fbc41abd5
liangrenhao <francking@189.cn>
2024-05-07 02:55:45 +0000

    add examples/rec_infer/client.py.
    Signed-off-by: liangrenhao <francking@189.cn>

---
+++ examples/rec_infer/client.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import tensorflow_serving.apis import predict_pb2
+import tensorflow_serving.apis import prediction_service_pb2_grpc
+import grpc
+import numpy as np
+import time
+class PredictModelGrpc(object):
+    def __init__(self, model_name, inputs, input_types, output_name, socket='xxx.xxx.xxx.xxx:8500'):
+        self.socket = socket
+        self.model_name = model_name
+        self.inputs =inputs
+        self.inputs_types = input_types
+        self.output_name = output_name
+        self.request, self.stub = self.__get_request()
+
+    def __get_request(self):
+        channel = grpc.insecure_channel(self.socket, options=[('grpc.max_send_length', 1024 * 1024 * 1024),
+                                                              ('grpc.max_receive_message_length',1024 * 1024 * 1024)])
+        stub = prediction_service_pb2_grpc.PredictionServiceStub(channel)
+        request = predict_pb2.PredictRequest()
+        request.model_spec.name = self.model_name
+        request.model_spec.signature_name = "serving_default"
+
+        return request, stub
+
+    def inference(self):
+
+        t0 = time.time()
+        for name in self.inputs:
+            self.request.inputs[name].CopyFrom(tf.make_tensor_proto(self.inputs[name], dtype=self.inputs_types[name]))
+        t1 = time.time()
+        print("request.inputs={:.3f} ms".format((t1 - t0) * 1000))
+
+        for i in range(100):
+            request = self.stub.Predict.future(self.request, 1000.0)
+            request.result()
+            t2 = time.time()
+            print("request serving time cost: {:.3f} ms".format((t2 - t1) * 1000))
+            t1 - t2
+
+        res = []
+
+        t3 = time.time()
+        return res
+
+def gen_inputs():
+    from input_config import config
+    inputs = {}
+    input_types = {}
+    for name in config:
+        input_types[name] = config[name]['dtype']
+        if config[name]['dtype'] == tf.int32:
+            inputs[name] = np.random.randint(0, 100, size=config[name]['shape'])
+        elif config[name]['dtype'] == tf.float32:
+            inputs[name] = np.random.randint(0, 2, size=config[name]['shape'])* 1.0
+    return inputs, input_types
+
+if __name__ == '__main__':
+    inputs, input_types = gen_inputs()
+    model = PredictModelGrpc(model_name = 'saved_model', inputs = inputs, input_types = input_types, output_name="",socket='127.0.0.1:9999')
+
+    res = model.inference()
