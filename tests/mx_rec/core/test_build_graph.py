6825609939059b481d24215e3bdb75892e653145
罗幸运 <luoqianke@126.com>
2024-05-08 19:59:08 +0800

    【修改说明 Modification】hot size静态修复

--- tests/mx_rec/core/test_build_graph.py
+++ tests/mx_rec/core/test_build_graph.py
@@ -32,12 +32,14 @@ class TestGetRestoreVectorFunc(unittest.TestCase):
     def setUp(self):
         # 默认动态扩容、hot emb、HBM
         self.config = dict(table_name="test_table", channel_id=0, is_hbm=True, emb_size=8, ext_emb_size=8,
-                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, use_dynamic_expansion=True)
+                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, device_id=0,
+                           use_dynamic_expansion=True)
 
     def tearDown(self):
         # 恢复config
         self.config = dict(table_name="test_table", channel_id=0, is_hbm=True, emb_size=8, ext_emb_size=8,
-                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, use_dynamic_expansion=True)
+                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, device_id=0,
+                           use_dynamic_expansion=True)
 
     def test_get_restore_vector_case1(self):
         """
@@ -112,13 +114,15 @@ class TestGetIdOffsetsFunc(unittest.TestCase):
     def setUp(self):
         # 默认动态扩容、hot emb、HBM
         self.config = dict(table_name="test_table", channel_id=0, is_hbm=True, emb_size=8, ext_emb_size=8,
-                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, use_dynamic_expansion=True)
+                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, device_id=0,
+                           use_dynamic_expansion=True)
         self.max_lookup_vec_size = self.config.get("send_count") * self.config.get("rank_size")
 
     def tearDown(self):
         # 恢复config
         self.config = dict(table_name="test_table", channel_id=0, is_hbm=True, emb_size=8, ext_emb_size=8,
-                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, use_dynamic_expansion=True)
+                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, device_id=0,
+                           use_dynamic_expansion=True)
 
     @mock.patch("mx_rec.core.asc.build_graph.npu_ops.gen_npu_ops.get_next")
     def test_get_id_offsets_case1(self, mock_get_next):
@@ -160,12 +164,14 @@ class TestGetAll2allArgsFunc(unittest.TestCase):
     def setUp(self):
         # 默认动态扩容、hot emb、HBM
         self.config = dict(table_name="test_table", channel_id=0, is_hbm=True, emb_size=8, ext_emb_size=8,
-                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, use_dynamic_expansion=True)
+                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, device_id=0,
+                           use_dynamic_expansion=True)
 
     def tearDown(self):
         # 恢复config
         self.config = dict(table_name="test_table", channel_id=0, is_hbm=True, emb_size=8, ext_emb_size=8,
-                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, use_dynamic_expansion=True)
+                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, device_id=0,
+                           use_dynamic_expansion=True)
 
     def test_get_all2all_args_case1(self):
         """
@@ -200,12 +206,14 @@ class TestGetPreProcessedTensorForAscFunc(unittest.TestCase):
     def setUp(self):
         # 默认动态扩容、hot emb、HBM
         self.config = dict(table_name="test_table", channel_id=0, is_hbm=True, emb_size=8, ext_emb_size=8,
-                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, use_dynamic_expansion=True)
+                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, device_id=0,
+                           use_dynamic_expansion=True)
 
     def tearDown(self):
         # 恢复config
         self.config = dict(table_name="test_table", channel_id=0, is_hbm=True, emb_size=8, ext_emb_size=8,
-                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, use_dynamic_expansion=True)
+                           feat_cnt=8, batch_size=32, rank_size=8, send_count=1, device_id=0,
+                           use_dynamic_expansion=True)
 
     @mock.patch.multiple("mx_rec.core.asc.build_graph",
                          get_restore_vector=mock.MagicMock(return_value=[0, 0]),
