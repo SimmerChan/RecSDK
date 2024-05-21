590578cf547e4efe38725e3f9614cb9f96ee6835
liangrenhao <francking@189.cn>
2024-05-21 06:50:41 +0000

    update examples/rec_infer/client.sh.
    Signed-off-by: liangrenhao <francking@189.cn>

--- examples/rec_infer/client.sh
+++ examples/rec_infer/client.sh
@@ -1,4 +1,4 @@
-export LD_LIBRARY_PATH=/usr/local/Ascend/ascend-toolkit/larest/lib64:/usr/local/Ascend/ascend-toolkit/larest/lib64/plugin/opskernel:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/Ascend/ascend-toolkit/latest/lib64:/usr/local/Ascend/ascend-toolkit/latest/lib64/plugin/opskernel:$LD_LIBRARY_PATH
 unset http_proxy
 unset https_proxy
 python3 client.py
