268c8440b067bf31a1fad863ad5a226de5a0f716
penghuiyang <1060916628@qq.com>
2024-05-28 10:01:40 +0800

    dlrm sess适配lazy_adam优化器

--- examples/dlrm/model/delay_loss_scale.py
+++ examples/dlrm/model/delay_loss_scale.py
@@ -17,32 +17,43 @@
 import tensorflow as tf
 from tensorflow.python.training import optimizer
 
+from config import Config
+
 
 class DenseLossScaleOptimizer:
-    def __init__(self, opt, loss_scale):
+    def __init__(self, opt: optimizer.Optimizer, loss_scale: int, cfg: Config) -> None:
         if not isinstance(opt, optimizer.Optimizer):
             raise ValueError('"opt" must be an instance of Optimizer, but got: %s' % type(opt))
         self._optimizer = opt
         self._loss_scale = tf.convert_to_tensor(loss_scale, tf.float32)
-        self._optimizer._learning_rate = self._optimizer._learning_rate / self._loss_scale
+        _divide_optim_by_loss_scale(self._optimizer, cfg, self._loss_scale)
 
     def compute_gradients(self, loss, var_list=None):
-        return self._optimizer.compute_gradients(loss*self._loss_scale, var_list=var_list)
+        return self._optimizer.compute_gradients(loss * self._loss_scale, var_list=var_list)
 
     def apply_gradients(self, avg_grads):
         return self._optimizer.apply_gradients(avg_grads)
 
 
 class SparseLossScaleOptimizer:
-    def __init__(self, opt, loss_scale):
+    def __init__(self, opt: optimizer.Optimizer, loss_scale: int, cfg: Config) -> None:
         if not isinstance(opt, optimizer.Optimizer):
             raise ValueError('"opt" must be an instance of Optimizer, but got: %s' % type(opt))
         self._optimizer = opt
         self._loss_scale = tf.convert_to_tensor(loss_scale, tf.float32)
-        self._optimizer._learning_rate = self._optimizer._learning_rate / self._loss_scale
+        _divide_optim_by_loss_scale(self._optimizer, cfg, self._loss_scale)
 
     def compute_gradients(self, loss, var_list=None):
-        return tf.gradients(loss*self._loss_scale, var_list)
+        return tf.gradients(loss * self._loss_scale, var_list)
 
     def apply_gradients(self, grads_and_vars):
         return self._optimizer.apply_gradients(grads_and_vars)
+
+
+def _divide_optim_by_loss_scale(opt, cfg, loss_scale):
+    if cfg.use_lazy_adam_optimizer:
+        # lazy_adam optimizer
+        opt._lr = opt._lr / loss_scale
+    else:
+        # SGD optimizer
+        opt._learning_rate = opt._learning_rate / loss_scale
--- examples/dlrm/model/optimizer.py
+++ examples/dlrm/model/optimizer.py
@@ -16,19 +16,30 @@
 
 import tensorflow as tf
 from delay_loss_scale import DenseLossScaleOptimizer, SparseLossScaleOptimizer
-from gradient_descent_w import create_hash_optimizer
+
 from mx_rec.util.initialize import ConfigInitializer
 from mx_rec.optimizers.gradient_descent_by_addr import create_hash_optimizer_by_addr
 
 
 def get_dense_and_sparse_optimizer(cfg):
-    dense_optimizer = tf.train.GradientDescentOptimizer(learning_rate=cfg.learning_rate[0])
     use_dynamic_expansion = ConfigInitializer.get_instance().use_dynamic_expansion
-    if use_dynamic_expansion:
-        sparse_optimizer = create_hash_optimizer_by_addr(learning_rate=cfg.learning_rate[1], weight_decay=0.0001)
+    if cfg.use_lazy_adam_optimizer:
+        if use_dynamic_expansion:
+            raise RuntimeError("model is incompatible with dynamic_expansion when use lazy_adam optimizer.")
+        # use lazy_adam optimizer
+        dense_optimizer = tf.compact.v1.train.AdamOptimizer(learning_rate=cfg.learning_rate[0])
+        from mx_rec.optimizers.lazy_adam import create_hash_optimizer
+        sparse_optimizer = create_hash_optimizer(learning_rate=cfg.learning_rate[1])
     else:
-        sparse_optimizer = create_hash_optimizer(learning_rate=cfg.learning_rate[1], weight_decay=0.0001)
-    sparse_optimizer = SparseLossScaleOptimizer(sparse_optimizer, 1024)
-    dense_optimizer = DenseLossScaleOptimizer(dense_optimizer, 1024)
+        # use SGD optimizer
+        dense_optimizer = tf.train.GradientDescentOptimizer(learning_rate=cfg.learning_rate[0])
+        if use_dynamic_expansion:
+            sparse_optimizer = create_hash_optimizer_by_addr(learning_rate=cfg.learning_rate[1], weight_decay=0.0001)
+        else:
+            from gradient_descent_w import create_hash_optimizer
+            sparse_optimizer = create_hash_optimizer(learning_rate=cfg.learning_rate[1], weight_decay=0.0001)
+
+    sparse_optimizer = SparseLossScaleOptimizer(sparse_optimizer, 1024, cfg)
+    dense_optimizer = DenseLossScaleOptimizer(dense_optimizer, 1024, cfg)
 
     return dense_optimizer, sparse_optimizer
