da38c520580ec650e1c6ca6d579642a9dd3bd376
steepcurve <steepcurve@163.com>
2024-07-16 17:18:42 +0800

    add fusion tracing

--- tools/perfrec-python/fusion_tracing.py
+++ tools/perfrec-python/fusion_tracing.py
@@ -1,14 +1,33 @@
 import argparse
 import re
+from collections import defaultdict
 from datetime import datetime
+from typing import Dict, List
+
+import toml
 
 
 class MxRecEvent:
     def __init__(self, log_line: str, event_name: str):
         timestamp_s = get_timestamp(log_line)
         duration_ms = get_duration(log_line, event_name)
+        process_id = get_process_id(log_line)
         self.timestamp_start_us = timestamp_s * 1e6 - float(duration_ms) * 1e3
         self.duration_us = float(duration_ms) * 1e3
+        self.process_id = process_id
+        self.name = event_name
+
+
+def extract_events(
+    log_path: str, event_names: Dict[str, str]
+) -> Dict[str, List[MxRecEvent]]:
+    events = defaultdict(list)
+    with open(log_path) as log:
+        for line in log:
+            for name in event_names.keys():
+                if name in line:
+                    events[event_names[name]].append(MxRecEvent(line, name))
+    return events
 
 
 def get_timestamp(log_line: str) -> float:
@@ -45,33 +64,26 @@ def get_process_id(log_line: str) -> int:
         raise RuntimeError(f"There is no process_id in log: {log_line}")
 
 
-def get_thread_id(log_line: str) -> int:
-    pattern = r"thread_id:\s*(\d+)"
-    match = re.search(pattern, log_line)
-    if match:
-        thread_id = match.group(1)
-        return int(thread_id)
-    else:
-        raise RuntimeError(f"There is no thread_id in log: {log_line}")
+def read_config() -> Dict[str, str]:
+    config = toml.load("config.toml")
+    mxrec_config = defaultdict(str)
+    for pipe, event_list in config["mxrec"].items():
+        for event in event_list:
+            mxrec_config[event] = pipe
+    return mxrec_config
 
 
 def main():
-    log = "[1,0]<stdout>:[MxRec][2024/7/15 16:28:5.148569] some other contents getAndSendTensorsTC(ms): 166."
-    print(get_timestamp(log))
-    print(get_duration(log, "getAndSendTensor"))
-    event = MxRecEvent(log, "getAndSendTensors")
-    print(event.timestamp_start_us, event.duration_us)
-
-    parser = argparse.ArgumentParser(description="Generate CPU/NPU fusion tracing json.")
+    parser = argparse.ArgumentParser(
+        description="Generate CPU/NPU fusion tracing json."
+    )
     parser.add_argument("debug_log", help="MxRec DEBUG level log.")
     args = parser.parse_args()
 
-    thread_ids = set()
-    with open(args.debug_log) as log:
-        for line in log:
-            if "getAndSendTensorsTC" in line:
-                thread_ids.add(get_thread_id(line))
-    print(thread_ids)
+    log_path = args.debug_log
+    config = read_config()
+    print(extract_events(log_path, config))
+
 
 if __name__ == "__main__":
     main()
