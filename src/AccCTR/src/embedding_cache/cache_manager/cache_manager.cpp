b0263ac6f6757c92fe3fa92873166e49b7eb29df
郭望 <1244372993@qq.com>
2024-07-22 15:56:09 +0800

    【FIX】修复DDR模式下，dlrm、DCNv2模型性能下降问题

--- src/AccCTR/src/embedding_cache/cache_manager/cache_manager.cpp
+++ src/AccCTR/src/embedding_cache/cache_manager/cache_manager.cpp
@@ -73,16 +73,15 @@ int EmbCacheManagerImpl::CreateCacheForTable(const EmbCacheInfo& embCacheInfo,
         return H_THREAD_NUM_ERROR;
     }
 
-    uint32_t reserveDevice = embCacheInfo.maxCacheSize / VOCAB_CACHE_RATIO;
-    if (!offsetMappers[embCacheInfo.tableName].Initialize(reserveDevice, embCacheInfo.maxCacheSize)) {
+    uint32_t reserve = embCacheInfo.vocabSize / VOCAB_CACHE_RATIO;
+    if (!offsetMappers[embCacheInfo.tableName].Initialize(reserve, embCacheInfo.maxCacheSize)) {
         offsetMappers[embCacheInfo.tableName].UnInitialize();
         offsetMappers.erase(embCacheInfo.tableName);
         return H_MEMORY_ALLOC_ERROR;
     }
 
     EmbPoolParam embPoolParam{prefillBufferSize, refillThreadNum};
-    uint32_t reserveHost = embCacheInfo.vocabSize / VOCAB_CACHE_RATIO;
-    if (!embTables[embCacheInfo.tableName].Initialize(embCacheInfo, reserveHost, initializerInfos, embPoolParam)) {
+    if (!embTables[embCacheInfo.tableName].Initialize(embCacheInfo, reserve, initializerInfos, embPoolParam)) {
         offsetMappers.erase(embCacheInfo.tableName);
         embTables.erase(embCacheInfo.tableName);
         return H_MEMORY_ALLOC_ERROR;
