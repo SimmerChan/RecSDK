9fb337b676d64c9272c84ef93e1c10846dac8f08
jiangli <jiangli67@huawei.com>
2024-07-25 11:03:14 +0800

    fix comments

--- cust_op/attention_fusion/op_host/attention_fusion.cpp
+++ cust_op/attention_fusion/op_host/attention_fusion.cpp
@@ -290,7 +290,6 @@ public:
         this->AICore()
             .SetTiling(optiling::TilingFunc);
         this->AICore().AddConfig("ascend910b");
-        this->AICore().AddConfig("ascend910");
     }
 };
 
