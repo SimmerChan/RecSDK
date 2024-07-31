4e081ae1954d86e4dd27dcff3a806f1d275deb4e
steepcurve <steepcurve@163.com>
2024-07-31 08:15:36 +0000

    add acl raii wrapper
    Signed-off-by: steepcurve <steepcurve@163.com>

---
+++ src/core/acl_raii/acl_raii.h
@@ -0,0 +1,61 @@
+/* Copyright 2024. Huawei Technologies Co.,Ltd. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+        limitations under the License.
+==============================================================================*/
+
+#ifndef MXREC_CORE_ACL_RAII_H_
+#define MXREC_CORE_ACL_RAII_H_
+
+#include "acl/acl_tdt.h"
+
+namespace MxRec {
+
+namespace Inner {
+aclError DestroyAclChannel(acltdtChannelHandle*);
+}
+
+template <typename T, aclError (*Fn)(T*)>
+class AclResourceRAII {
+public:
+    AclResourceRAII() = delete;
+    explicit AclResourceRAII(T* dataset) : inner_(dataset) {}
+    ~AclResourceRAII()
+    {
+        if (this->inner_ != nullptr) {
+            Fn(this->inner_);
+            this->inner_ = nullptr;
+        }
+    }
+    AclResourceRAII(const AclResourceRAII& rhs) = delete;
+    AclResourceRAII& operator=(const AclResourceRAII& rhs) = delete;
+    AclResourceRAII(AclResourceRAII&& rhs) = delete;
+    AclResourceRAII& operator=(AclResourceRAII&& rhs) = delete;
+
+    T* ptr()
+    {
+        return this->inner_;
+    }
+
+private:
+    T* inner_;
+};
+
+/// RAII wrapper for `acltdtDataset*`
+using AcltdtDataset = AclResourceRAII<acltdtDataset, acltdtDestroyDataset>;
+
+/// RAII wrapper for `acltdtChannelHandle*`
+using AcltdtChannelHandle = AclResourceRAII<acltdtChannelHandle, Inner::DestroyAclChannel>;
+
+}  // namespace MxRec
+
+#endif
