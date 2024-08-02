61dd20d2c446b8aa4e3258ac736b855c60dcd24d
penghuiyang <1060916628@qq.com>
2024-08-02 18:16:18 +0800

    【FIX】修复第0步保存数据错误问题

--- src/tests/emb_table/embedding_static_test.cpp
+++ src/tests/emb_table/embedding_static_test.cpp
@@ -156,7 +156,7 @@ TEST_F(EmbeddingStaticTest, SaveKeyData)
     vector<EmbInfo> embInfos = {embInfo_};
     shared_ptr<EmbeddingStatic> hbm = std::make_shared<EmbeddingStatic>(embInfo_, rankInfo_, 0);
     hbm->SetFileSystemPtr("test_dir");
-    hbm->Save("test_dir");
+    hbm->Save("test_dir", 1);
     bool fileExist = false;
     if (access("./test_dir/test1/key", F_OK) == 0) {
         fileExist = true;
