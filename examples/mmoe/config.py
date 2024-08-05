486538ea0b4247f2a3ec37877653adf45c403a89
无悔 <1187940490@qq.com>
2024-08-05 19:43:55 +0800

    clean code

--- examples/mmoe/config.py
+++ examples/mmoe/config.py
@@ -97,8 +97,23 @@ class Config:
             return 46000 // rank_size
         except ZeroDivisionError as exp:
             raise ZeroDivisionError('Rank size can not be zero.') from exp
-         
+        
 
+    def get_emb_table_cfg(self) -> None:
+        if self.cache_mode == CacheModeEnum.HBM.value:
+            return {"device_vocabulary_size": self.dev_vocab_size}
+        elif self.cache_mode == CacheModeEnum.DDR.value:
+            return {"device_vocabulary_size": self.dev_vocab_size,
+                    "host_vocabulary_size": self.host_vocab_size}
+        elif self.cache_mode == CacheModeEnum.SSD.value:
+            return {"device_vocabulary_size": self.dev_vocab_size,
+                    "host_vocabulary_size": self.host_vocab_size,
+                    "ssd_vocabulary_size": self.ssd_vocab_size,
+                    "ssd_data_path": SSD_DATA_PATH}
+        else:
+            raise RuntimeError(f"get CACHE_MODE:{self.cache_mode}, check Config.__set_emb_table_size implementation")
+        
+    
     def __set_emb_table_size(self) -> None:
         self.cache_mode = os.getenv("CACHE_MODE")
         if self.cache_mode is None:
@@ -116,20 +131,6 @@ class Config:
             self.ssd_vocab_size = 1000 * self.rank_size
         else:
             raise ValueError(f"get CACHE_MODE:{self.cache_mode}, expect in [HBM, DDR, SSD]")
-
-    def get_emb_table_cfg(self) -> None:
-        if self.cache_mode == CacheModeEnum.HBM.value:
-            return {"device_vocabulary_size": self.dev_vocab_size}
-        elif self.cache_mode == CacheModeEnum.DDR.value:
-            return {"device_vocabulary_size": self.dev_vocab_size,
-                    "host_vocabulary_size": self.host_vocab_size}
-        elif self.cache_mode == CacheModeEnum.SSD.value:
-            return {"device_vocabulary_size": self.dev_vocab_size,
-                    "host_vocabulary_size": self.host_vocab_size,
-                    "ssd_vocabulary_size": self.ssd_vocab_size,
-                    "ssd_data_path": SSD_DATA_PATH}
-        else:
-            raise RuntimeError(f"get CACHE_MODE:{self.cache_mode}, check Config.__set_emb_table_size implementation")
 
 
 def sess_config(dump_data=False, dump_path="./dump_output", dump_steps="0|1|2"):
