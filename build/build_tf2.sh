1b2b6235ab31c63b966d266d3fa23aad0362de72
steepcurve <steepcurve@163.com>
2024-08-13 19:56:47 +0800

    fix retry

--- build/build_tf2.sh
+++ build/build_tf2.sh
@@ -36,7 +36,26 @@ fi
 
 function install_expected(){
   cd "$MxRec_DIR"
-  git submodule init && git submodule update
+  max_attempts=5
+  attempt=1
+  until [ $attempt -gt $max_attempts ]
+  do
+    git submodule init && git submodule update
+    result=$?
+    if [[ $result -eq 0 ]]
+    then
+      break   
+    else
+      echo "Attempt $attempt to install cpp-expected failed! Trying again in 10 seconds..."
+      attempt=$((attempt + 1))
+      sleep 10
+    fi
+  done
+
+  if [ $attempt -gt $max_attempts ]; then
+      echo "Failed to install cpp-expected after $max_attempts attempts."
+      exit 1
+  fi
   cd -
 }
 
@@ -119,26 +138,7 @@ function collect_so_file()
 
 # start to build MxRec
 echo "----------------          install     expected          ----------------"
-max_attempts=5
-attempt=1
-until [ $attempt -gt $max_attempts ]
-do
-  install_expected
-  result=$?
-  if [[ $result -eq 0 ]]
-  then
-    break   
-  else
-    echo "Attempt $attempt to install cpp-expected failed! Trying again in 10 seconds..."
-    attempt=$((attempt + 1))
-    sleep 10
-  fi
-done
-
-if [ $attempt -gt $max_attempts ]; then
-    echo "Failed to install cpp-expected after $max_attempts attempts."
-    exit 1
-fi
+install_expected
 echo "----------------          compile     securec           ----------------"
 compile_securec
 echo "----------------          compile     AccCTR            ----------------"
