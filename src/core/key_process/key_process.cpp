5de2597428817075496e138748dacb3c0105dd40
何霖 <helin_1@163.com>
2024-08-16 09:10:05 +0800

    【FIX】清理cleancode

--- src/core/key_process/key_process.cpp
+++ src/core/key_process/key_process.cpp
@@ -556,7 +556,7 @@ bool KeyProcess::KeyProcessTaskHelper(unique_ptr<EmbBatchT>& batch, int channel,
         FeatureAdmitAndEvict::m_embStatus[batch->name] != SingleEmbTableStatus::SETS_NONE) {
         countRecv = GetCountRecv(batch, threadId, keyCount, scAll, ss);
     }
-    if (isIncrementalCheckpoint && !channel) {
+    if (isIncrementalCheckpoint && channel == 0) {
         countRecv = GetCountRecv(batch, threadId, keyCount, scAll, ss);
         map<emb_key_t, emb_key_t> tmpKeyCountMap;
         auto keySize = lookupKeys.size();
@@ -596,7 +596,7 @@ bool KeyProcess::KeyProcessTaskHelper(unique_ptr<EmbBatchT>& batch, int channel,
 
     // 将keyCountVec放进tensor里并推到一个队列里
     auto keyCountTensors = make_unique<vector<Tensor>>();
-    if (isIncrementalCheckpoint && !channel) {
+    if (isIncrementalCheckpoint && channel == 0) {
         keyCountTensors->push_back(Vec2TensorI64(keyCountVec));
     }
 
@@ -607,7 +607,7 @@ bool KeyProcess::KeyProcessTaskHelper(unique_ptr<EmbBatchT>& batch, int channel,
         PushGlobalUniqueTensors(tensors, lookupKeys, channel);
         tensors->push_back(rankInfo.useDynamicExpansion ? Vec2TensorI64(lookupKeys) : Vec2TensorI32(lookupKeys));
         PushResultHBM(batch, move(tensors));
-        if (isIncrementalCheckpoint && !channel) {
+        if (isIncrementalCheckpoint && channel == 0) {
             PushKeyCountHBM(batch, move(keyCountTensors));
         }
     } else {
