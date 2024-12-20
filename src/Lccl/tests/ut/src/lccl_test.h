e6757eee359011b171b4d99a245f2d35eea1d8e7
ChinaLinZhen <351668265@qq.com>
2024-12-20 10:56:29 +0800

    add lccl gtest

---
+++ src/Lccl/tests/ut/src/lccl_test.h
@@ -0,0 +1,46 @@
+/* Copyright (c) Huawei Technologies Co., Ltd. 2022-2024. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ==============================================================================*/
+
+#ifndef OCK_UNIQUE_TEST_H
+#define OCK_UNIQUE_TEST_H
+
+#include <mpi.h>
+#include <gtest/gtest.h>
+#include <vector>
+#include <unordered_set>
+#include <map>
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "lcal_comm.h"
+
+
+using namespace std;
+using namespace Lcal;
+
+
+class LcclTest : public testing::Test {
+protected:
+    LcclTest() {};
+    ~LcclTest() {};
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+
+
+    void SetUp() {}
+
+    void TearDown() {}
+};
+
+
+#endif // OCK_UNIQUE_TEST_H
