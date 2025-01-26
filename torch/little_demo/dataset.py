6a8dfafef46c4ef0a362f0d357f2172661cfba31
wuhongfa <1660398197@qq.com>
2025-01-26 09:39:29 +0000

    [feat]clean code

--- torch/little_demo/dataset.py
+++ torch/little_demo/dataset.py
@@ -1,13 +1,27 @@
+# coding: UTF-8
+# Copyright 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import itertools
 from typing import Iterator
 from dataclasses import dataclass
 
 import torch_npu
-import torch
 from torch.utils.data.dataset import IterableDataset
 from torchrec.streamable import Pipelineable
 from torchrec import KeyedJaggedTensor, JaggedTensor
-
+import torch
 
 @dataclass
 class Batch(Pipelineable):
--- torch/little_demo/test_hybrid.py
+++ torch/little_demo/test_hybrid.py
@@ -1,8 +1,21 @@
+# coding: UTF-8
+# Copyright 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 import os
 import logging
 
-import torch.distributed as dist
-from torch.utils.data import DataLoader
 from torchrec.optim.keyed import CombinedOptimizer
 from torchrec.distributed import DistributedModelParallel
 from torchrec.distributed.types import ShardingEnv
@@ -21,6 +34,8 @@ from hybrid_torchrec.distributed.sharding.hybrid_embeddingbag import (
 
 from dataset import RandomRecDataset
 from model import TestModel
+import torch.distributed as dist
+from torch.utils.data import DataLoader
 import torch
 
 
