5f7ae320c109330115fa6eb07c8478a36d7d0e69
tanfeng <823018000@qq.com>
2025-04-30 17:03:06 +0800

    torchrec npu适配

--- torchrec/build_whl.sh
+++ torchrec/build_whl.sh
@@ -5,14 +5,22 @@ set -e
 
 VERSION="1.1.0-npu"
 ARCH=$(uname -m)
+
+current_dir="$(pwd)"
+torchrec_path="$current_dir/torchrec"
 package_name="Ascend-mindxsdk-torchrec-"${VERSION}"-linux-"${ARCH}".tar.gz"
 
-# 依赖torchrec源码,版本固定为1.1.0
+# 检查目录是否存在
+if [ -d "$torchrec_path" ]; then
+  rm -rf "$torchrec_path"
+fi
+
+# 依赖torchrec源码,版本固定为1.1.0，提交hash固定为2c5f6ee。
 git clone -b release/v1.1.0 https://github.com/pytorch/torchrec.git
 cd torchrec && git checkout 2c5f6ee
 # patch
-cp ../torchrec_npu.path ./
-git apply torchrec_npu.path
+cp ../torchrec_npu.patch ./ && dos2unix torchrec_npu.patch
+git apply torchrec_npu.patch
 
 # 编译安装包
 if [ -f "${package_name}"]; then
