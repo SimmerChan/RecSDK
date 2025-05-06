01b1ff5985acdc2003d8d9cba4846c37ef779dd0
tanfeng <823018000@qq.com>
2025-05-06 16:28:10 +0800

    流水线git下载源码网络问题

--- torchrec/build_whl.sh
+++ torchrec/build_whl.sh
@@ -11,13 +11,13 @@ package_name="Ascend-mindxsdk-torchrec-"${VERSION}"-linux-"${ARCH}".tar.gz"
 
 # 依赖torchrec源码,版本固定为1.1.0，提交hash固定为2c5f6ee，避免网络不稳定问题，流水线下载好。
 # git clone -b release/v1.1.0 https://github.com/pytorch/torchrec.git
-cd torchrec && git checkout 2c5f6ee
+cd torchrec
 # patch
 cp ../torchrec_npu.patch ./ && dos2unix torchrec_npu.patch
 git apply torchrec_npu.patch
 
 # 编译安装包
-if [ -f "${package_name}"]; then
+if [ -f "${package_name}" ]; then
   rm "${package_name}"
 fi
 python3 setup.py bdist_wheel --plat-name linux_"${ARCH}"
