4275f8f3bfaed17b84dbd6450657c86a13c218d4
xiajunhua <xiajunhua1@huawei.com>
2025-05-07 17:08:19 +0800

    fix cleancode

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/asynchronous_complete_cumsum/test_asynchronous_complete_cumsum.py
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/asynchronous_complete_cumsum/test_asynchronous_complete_cumsum.py
@@ -15,12 +15,12 @@
 # limitations under the License.
 # ==============================================================================
 
+from pathlib import Path
 import pytest
 import torch
 import torch_npu
 import fbgemm_gpu
 import numpy as np
-from pathlib import Path
 
 CURR_DIR = Path(__file__).resolve().parent
 torch.ops.load_library(str(CURR_DIR.parent.parent /
