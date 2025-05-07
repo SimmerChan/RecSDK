40b041b4e64564904722876302fab37758b6485f
xiajunhua <xiajunhua1@huawei.com>
2025-05-07 18:12:37 +0800

    add permute op

---
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/permute2d_sparse_data/test_permute2d_sparse_data.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import pytest
+import torch
+import torch_npu
+import fbgemm_gpu
+import numpy as np
+
+CURR_DIR = Path(__file__).resolve().parent
+torch.ops.load_library(str(CURR_DIR.parent.parent /
+    "torch_library/2.6.0/permute2d_sparse_data/build/libpermute2d_sparse_data.so"))
+
+lengths_type = [np.int64, np.int32, np.int64, np.int32]
+values_type = [np.int64, np.int32, np.float32, np.float32]
+
+
+def get_result(permute, lengths, values):
+    input_permute_torch = torch.from_numpy(permute)
+    input_lengths_torch = torch.from_numpy(lengths)
+    input_values_torch = torch.from_numpy(values)
+
+    (permuted_lengths, permuted_values, permuted_weights) = (
+        torch.ops.fbgemm.permute_2D_sparse_data(
+            input_permute_torch,
+            input_lengths_torch,
+            input_values_torch,
+        )
+    )
+
+    return permuted_lengths.cpu(), permuted_values.cpu()
+
+
+def get_result_npu(permute, lengths, values, device="npu:0"):
+    torch.npu.set_device(device)
+    input_permute_torch = torch.from_numpy(permute).to(device)
+    input_lengths_torch = torch.from_numpy(lengths).to(device)
+    input_values_torch = torch.from_numpy(values).to(device)
+
+    (permuted_lengths, permuted_values, permuted_weights) = (
+        torch.ops.fbgemm.permute_2D_sparse_data(
+            input_permute_torch, input_lengths_torch, input_values_torch, None
+        )
+    )
+    torch.npu.synchronize()
+    return permuted_lengths.cpu(), permuted_values.cpu()
+
+
+@pytest.mark.parametrize("device", ["npu:0", "npu:5"])
+@pytest.mark.parametrize("type_list", zip(lengths_type, values_type))
+@pytest.mark.parametrize("permute_dim", np.random.randint(2, 30, 4).tolist())
+@pytest.mark.parametrize("lengths", [2048, 20480, 204800])
+def test_permute2d_sparse_data(type_list, device, permute_dim, lengths):
+    ltype, vtype = type_list
+    input_permute = np.arange(permute_dim).astype(np.int32)
+    np.random.shuffle(input_permute)
+    input_lengths = np.ones((permute_dim, lengths), dtype=ltype)
+    input_values = np.arange(0, permute_dim * lengths).astype(vtype)
+
+    golden = get_result(input_permute, input_lengths, input_values)
+    result = get_result_npu(input_permute, input_lengths, input_values, device)
+
+    assert torch.allclose(golden[0], result[0], atol=1e-5)
+    assert torch.allclose(golden[1], result[1], atol=1e-5)
