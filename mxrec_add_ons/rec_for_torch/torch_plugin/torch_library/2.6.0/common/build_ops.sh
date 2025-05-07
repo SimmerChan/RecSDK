813201d89ac9e13afafac583299e1562a0679916
xiajunhua <xiajunhua1@huawei.com>
2025-05-07 10:05:12 +0800

    add op common

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/common/build_ops.sh
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/common/build_ops.sh
@@ -17,4 +17,14 @@ rm -rf build
 mkdir -p build
 cmake -B build
 cmake --build build -j
-export LD_LIBRARY_PATH=$ASCEND_OPP_PATH/vendors/permute2d_sparse_data/op_api/lib:$LD_LIBRARY_PATH
+
+#默认放在python3,site-package目录下
+PACKAGE_PATH=$(python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))")
+if [ -d "$PACKAGE_PATH" ]; then
+  echo "build to: $PACKAGE_PATH"
+  cp ./build/*.so ${PACKAGE_PATH}/
+else
+  echo "build to: ${PWD}/build"
+fi
+
+export LD_LIBRARY_PATH=$ASCEND_OPP_PATH/vendors/FbgemmNpuApi/op_api/lib:$LD_LIBRARY_PATH
