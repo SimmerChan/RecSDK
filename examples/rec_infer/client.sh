6f01adb43cc78a1b90ddbe2ac564c2e446c490f5
李大雄 <lixiong51@huawei.com>
2025-05-14 11:20:51 +0800

    特性POC分支 删除rec相关代码

--- examples/rec_infer/client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
-# Description: startup client
-
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
-unset http_proxy
-unset https_proxy
-python3 client.py $1
