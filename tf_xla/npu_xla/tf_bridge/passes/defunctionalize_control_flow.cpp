334512a8e366604a3935a756a3c745b69460693d
steepcurve <steepcurve@163.com>
2025-05-15 10:17:24 +0800

    feat(tf_bridge): 添加NPU XLA优化和构建操作的相关代码
    新增了NPU XLA优化和构建操作的相关代码，包括优化传递、控制流去功能化、NPU XLA操作构建等功能。这些改动旨在提升NPU XLA在TensorFlow中的集成和优化能力，支持更高效的模型执行。

---
+++ tf_xla/npu_xla/tf_bridge/passes/defunctionalize_control_flow.cpp
@@ -0,0 +1,56 @@
+/* Copyright 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+        limitations under the License.
+==============================================================================*/
+// Copyright 2021 The BladeDISC Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tf_bridge/passes/defunctionalize_control_flow.h"
+
+#include "tensorflow/core/framework/function.h"
+#include "tensorflow/core/graph/graph.h"
+#include "tf_bridge/tf/lower_if_op.h"
+#include "tf_bridge/tf/lower_while_op.h"
+
+namespace tensorflow {
+namespace npu_xla {
+
+Status DefunctionalizeFactory::defunctionalize(Node* n, Graph* g, const FunctionLibraryDefinition& flib)
+{
+    int num_old = g->num_op_nodes();
+    auto defunctionalize = defunctionalize_factory_.find(n->type_string());
+    if (defunctionalize != defunctionalize_factory_.end()) {
+        TF_RETURN_IF_ERROR(defunctionalize->second(n, g, flib));
+    }
+    accum_defunc_ += (g->num_op_nodes() - num_old);
+    return absl::OkStatus();
+}
+
+void DefunctionalizeFactory::Initialize()
+{
+    defunctionalize_factory_["While"] = RewriteWhileNode;
+    defunctionalize_factory_["If"] = RewriteIfNode;
+}
+
+int DefunctionalizeFactory::accum_defunc_ = 0;
+
+}  // namespace npu_xla
+}  // namespace tensorflow
---
+++ tf_xla/npu_xla/tf_bridge/passes/npu_xla_optimization_pass.cpp
@@ -0,0 +1,64 @@
+/* Copyright 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+        limitations under the License.
+==============================================================================*/
+
+#include "tf_bridge/passes/npu_xla_optimization_pass.h"
+
+#include "tf_bridge/passes/build_npu_xla_op_pass.h"
+#include "tf_bridge/passes/encapsulate_subgraphs_pass.h"
+#include "tf_bridge/passes/mark_for_npu_compilation_pass.h"
+#include "tf_bridge/utils/dump_graph.h"
+
+namespace tensorflow {
+namespace npu_xla {
+
+namespace {
+void DumpGraph(const GraphOptimizationPassOptions& options, const char* const name)
+{
+    if (VLOG_IS_ON(2)) {
+        Graph* graph = options.graph->get();
+        auto dumped = dump_graph::DumpGraphToFile(name, *graph, options.flib_def);
+        VLOG(2) << "NpuXlaOptimizationPass dump graph: " << dumped;
+    }
+}
+
+}  // namespace
+
+NpuXlaOptimizationPass::NpuXlaOptimizationPass() {}
+Status NpuXlaOptimizationPass::Run(const GraphOptimizationPassOptions& options)
+{
+    DumpGraph(options, "before_npu_xla_optimization_pass");
+
+    MarkForNpuCompilationPass mark_pass;
+    mark_pass.set_name("MarkForNpuCompilationPass");
+    TF_RETURN_IF_ERROR(mark_pass.Run(options));
+
+    EncapsulateSubgraphsPass encap_pass;
+    encap_pass.set_name("EncapsulateSubgraphsPass");
+    TF_RETURN_IF_ERROR(encap_pass.Run(options));
+
+    DumpGraph(options, "before_build_npu_xla_op_pass");
+
+    BuildNpuXlaOpPass op_pass;
+    op_pass.set_name("BuildNpuXlaOpPass");
+    TF_RETURN_IF_ERROR(op_pass.Run(options));
+
+    DumpGraph(options, "after_npu_xla_optimization_pass");
+
+    return OkStatus();
+}
+
+REGISTER_OPTIMIZATION(OptimizationPassRegistry::POST_REWRITE_FOR_EXEC, 0, NpuXlaOptimizationPass);
+}  // namespace npu_xla
+}  // namespace tensorflow
