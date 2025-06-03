ce2f6d4d97b4973df24393933bb0fcc1ab35ec08
zhoucy <zhoucy98@126.com>
2025-06-03 11:27:13 +0800

    [feat]relative_attn_bias_pos

--- mxrec_add_ons/rec_for_torch/operators/relative_attn_bias_pos/op_kernel/rab_common.h
+++ mxrec_add_ons/rec_for_torch/operators/relative_attn_bias_pos/op_kernel/rab_common.h
@@ -12,8 +12,6 @@
 constexpr int DATA_ALIGN_BYTES = 32;
 constexpr int MAX_BATCH_SIZE = 512;
 constexpr int NUM_BUFFER = 2;
-constexpr int MAX_SEQ_CNT = 128;
-constexpr int GATHER_PROCESS_WINDOW = 4096;
 
 constexpr int8_t TYPE_FP32 = 0;
 constexpr int8_t TYPE_FP16 = 1;
@@ -26,12 +24,8 @@ struct Args {
     // pos_bias
     GM_ADDR positionBias;
     GM_ADDR identity;
-    // ts_bias
-    GM_ADDR timestamps;
-    GM_ADDR timestampsWeights;
     // out
     GM_ADDR rabPosOut;
-    GM_ADDR rabTimeOut;
 
     GM_ADDR workspace;
     GM_ADDR tiling;
