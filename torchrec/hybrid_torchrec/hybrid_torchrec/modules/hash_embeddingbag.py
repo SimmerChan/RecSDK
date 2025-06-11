48cc4463d7b06c629b4054a581601b89a54ebb30
whf <1660398197@qq.com>
2025-06-11 15:53:36 +0800

    param check

--- torchrec/hybrid_torchrec/hybrid_torchrec/modules/hash_embeddingbag.py
+++ torchrec/hybrid_torchrec/hybrid_torchrec/modules/hash_embeddingbag.py
@@ -23,7 +23,7 @@ from torchrec.modules.embedding_configs import (
     DataType,
     EmbeddingBagConfig,
     pooling_type_to_str,
-    PoolingType
+    PoolingType,
 )
 from torchrec.modules.embedding_modules import (
     EmbeddingBagCollectionInterface,
@@ -87,18 +87,23 @@ class HashTableOption:
 class HashEmbeddingBagConfig(EmbeddingBagConfig):
     pass
 
+
 def is_valid_feat_name(feat_name):
     for char in feat_name:
-        if not (char.isalnum() or char == '_'):
+        if not (char.isalnum() or char == "_"):
             return False
     return True
+
 
 def check_embedding_config_valid(config: HashEmbeddingBagConfig):
     if config.embedding_dim % EMBEDDINGS_DIM_ALIGNMENT != 0:
         raise ValueError(
             f"The embedding dim should be a multiple of 8, but is {config.embedding_dim}"
         )
-    if config.embedding_dim < EMBEDDINGS_DIM_ALIGNMENT or config.embedding_dim > MAX_EMBEDDINGS_DIM:
+    if (
+        config.embedding_dim < EMBEDDINGS_DIM_ALIGNMENT
+        or config.embedding_dim > MAX_EMBEDDINGS_DIM
+    ):
         raise ValueError(
             f"The embedding dim should be in [{EMBEDDINGS_DIM_ALIGNMENT}, {MAX_EMBEDDINGS_DIM}], but is {config.embedding_dim}"
         )
@@ -107,22 +112,20 @@ def check_embedding_config_valid(config: HashEmbeddingBagConfig):
             f"The embedding dim should be in [1, {MAX_NUM_EMBEDDINGS}], but is {config.num_embeddings}"
         )
     if config.data_type != DataType.FP32:
-        raise ValueError(
-            f"The data_type should be FP32, but is {config.data_type}"
-        )
-    if config.feature_names is None or len(config.feature_names) == 0 :
+        raise ValueError(f"The data_type should be FP32, but is {config.data_type}")
+    if config.feature_names is None or len(config.feature_names) == 0:
         raise ValueError(
             f"The feature_names should not be empty, but is {config.feature_names}"
         )
     for feat_name in config.feature_names:
-        if (not is_valid_feat_name(feat_name)):
-         raise ValueError(
-            f"The feature_name should contain a-Z, 0-9, _, but is {feat_name}"
-        ) 
+        if not is_valid_feat_name(feat_name):
+            raise ValueError(
+                f"The feature_name should contain a-Z, 0-9, _, but is {feat_name}"
+            )
     if config.weight_init_max is not None:
         raise ValueError(
             f"The config.weight_init_max should be None, but is {config.weight_init_max}"
-        )    
+        )
     if config.weight_init_min is not None:
         raise ValueError(
             f"The config.weight_init_min should be None, but is {config.weight_init_min}"
@@ -130,12 +133,15 @@ def check_embedding_config_valid(config: HashEmbeddingBagConfig):
     if config.num_embeddings_post_pruning is not None:
         raise ValueError(
             f"The config.num_embeddings_post_pruning should be None, but is {config.num_embeddings_post_pruning}"
-        )   
+        )
     if config.init_fn is not None and not hasattr(config.init_fn, "__call__"):
         raise ValueError(
             f"The config.init_fn should be callable, but is {config.init_fn}"
         )
-    if config.pooling is not None and config.pooling not in [PoolingType.SUM, PoolingType.MEAN]:
+    if config.pooling is not None and config.pooling not in [
+        PoolingType.SUM,
+        PoolingType.MEAN,
+    ]:
         raise ValueError(
             f"The config.pooling should be in [PoolingType.SUM, PoolingType.MEAN], but is {config.pooling}"
         )
