4db782ffa484401efcdb660e48ac980655be8c6c
Taoqiuyu <1042656162@qq.com>
2025-06-13 16:56:13 +0800

    删除table-wise

--- torchrec/hybrid_torchrec/test/test_hybrid_pipline_hash_embeddingbag.py
+++ torchrec/hybrid_torchrec/test/test_hybrid_pipline_hash_embeddingbag.py
@@ -235,7 +235,7 @@ class TestModel:
 @pytest.mark.parametrize("embedding_dims", [[32, 64, 128]])
 @pytest.mark.parametrize("num_embeddings", [[400, 4000, 400]])
 @pytest.mark.parametrize("pool_type", [torchrec.PoolingType.MEAN])
-@pytest.mark.parametrize("sharding_type", ["table_wise", "row_wise"])
+@pytest.mark.parametrize("sharding_type", ["row_wise"])
 @pytest.mark.parametrize("lookup_len", [1024])
 @pytest.mark.parametrize("device", ["npu"])
 @pytest.mark.parametrize("optim", [Adagrad])
