aab1404dadc72e77a2191d68d3ee488133a3fd7b
马千里 <949120498@qq.com>
2025-06-27 10:59:33 +0800

    clean code fix: 降低代码重复率1.2

--- examples/rec_model_zoo/behaviour_and_multi_task/src/models/sharedbottom.py
+++ examples/rec_model_zoo/behaviour_and_multi_task/src/models/sharedbottom.py
@@ -14,7 +14,12 @@ import pytz
 import tensorflow as tf
 from npu_bridge.npu_init import NPUEstimator, NPURunConfig
 
-from utils import get_third_nearest_checkpoint, json_file_load, dump_pred_multi
+from utils import (
+    get_third_nearest_checkpoint,
+    json_file_load,
+    dump_pred_multi,
+    embedding_lookup_sparse_fake
+)
 
 tf.compat.v1.enable_control_flow_v2()
 tf.compat.v1.enable_resource_variables()
@@ -111,18 +116,6 @@ def build_optimizer(model_cfg) -> tf.compat.v1.train.Optimizer:
 
 def model_fn(features, labels, mode, params):
     """build Estimator model"""
-
-    def embedding_lookup_sparse_fake(params, ids, combiner=None, name=None):
-        dense_mask = tf.expand_dims(tf.cast(ids >= 0, tf.float32), axis=-1)
-        ids = tf.where(tf.equal(ids, -1), tf.zeros_like(ids), ids)
-        embedding = tf.nn.embedding_lookup(params, ids, name=name + "_dense_lookup") * dense_mask
-        summed_embedding = tf.reduce_sum(embedding, axis=1)
-        if combiner == "sum":
-            return summed_embedding
-        elif combiner == "mean":
-            return summed_embedding / tf.reduce_sum(dense_mask, axis=1)
-        else:
-            raise ValueError("combiner only supoort 'sum', 'mean'")
 
     with tf.compat.v1.variable_scope("Embedding-Layer"):
         emb_weights = {}
