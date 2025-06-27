1254f12696dd2f1e5a24709efa038bebdc9f3dee
whf <1660398197@qq.com>
2025-06-27 17:00:06 +0800

    clean code

--- torchrec/torchrec_embcache/src/torchrec_embcache/distributed/sharding/embedding_sharder.py
+++ torchrec/torchrec_embcache/src/torchrec_embcache/distributed/sharding/embedding_sharder.py
@@ -90,10 +90,7 @@ class EmbCacheEmbeddingBagCollectionSharder(EmbeddingBagCollectionSharder):
         )
 
     def sharding_types(self, compute_device_type: str) -> List[str]:
-        # # compute_device_type 只支持cpu
-        # if compute_device_type in {"mtia", "cuda", "npu"}:
-        #     return []
-
+        # compute_device_type 只支持cpu
         types = [
             ShardingType.ROW_WISE.value,
             # 以下 ShardingType 待实现
