d8ce84a83c77c4baf31b2772eb93aa08ca552194
wuyangjian <wuyangjian1115@163.com>
2025-06-27 16:36:59 +0800

    st dt test代码合入part4

--- torchrec/torchrec_embcache/st_dt_test/dt/test_compute_and_output_dist.py
+++ torchrec/torchrec_embcache/st_dt_test/dt/test_compute_and_output_dist.py
@@ -201,15 +201,15 @@ def execute(rank, config):
 
     fuse_input_dist_splits(context)
 
+    kjt_list_dict = {}
     for names, awaitable in context.fused_splits_awaitables:
         for name, request in zip(names, awaitable.wait()):
-            context.input_dist_tensors_requests[name] = AwaitableAdapter(request)
+            kjt_list_dict[name] = request.awaitables
 
     jt_list = []
     for i, module in enumerate(module_lst):
         name = f"module.{i}"
-        awaitable = context.input_dist_tensors_requests[name]
-        kjt_lst = awaitable.wait()
+        kjt_lst = kjt_list_dict[name]
         post_waitable = module.post_input_dist(
             context.module_contexts[name], 
             kjt_lst
