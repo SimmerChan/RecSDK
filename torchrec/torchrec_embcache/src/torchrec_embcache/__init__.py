d41a2a9f3f081def7f8fb5486037b57b47b3c348
wuyangjian <wuyangjian1115@163.com>
2025-06-28 17:29:44 +0800

    src公共代码代码合入

--- torchrec/torchrec_embcache/src/torchrec_embcache/__init__.py
+++ torchrec/torchrec_embcache/src/torchrec_embcache/__init__.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) Huawei Platforms, Inc. and affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 import logging
 import os
 import sys
@@ -14,7 +21,7 @@ if current_dir not in sys.path:
     sys.path.append(current_dir)
 
 try:
-    from . import embcache_pybind
+    import embcache_pybind
     logging.debug("Successfully imported embcache_pybind from %s", current_dir)
 except ImportError as e:
     logging.error("Error importing embcache_pybind from %s: %s", current_dir, e)
