1d13d47b85684bb86aa115d81db5b5e7b342eb25
wuyangjian <wuyangjian1115@163.com>
2025-06-28 17:34:48 +0800

    3part代码合入

--- torchrec/torchrec_embcache/src/3rdparty/produce_git_modules.sh
+++ torchrec/torchrec_embcache/src/3rdparty/produce_git_modules.sh
@@ -4,15 +4,15 @@
 #-------------------------------------
 git submodule deinit googletest
 git rm --cached googletest
-git clone -b release-1.8.1 https://codehub-dg-y.huawei.com/OpenSourceCenter/googletest.git 
-git submodule add -b release-1.8.1 https://codehub-dg-y.huawei.com/OpenSourceCenter/googletest.git googletest
+git clone -b release-1.8.1 https://github.com/google/googletest
+git submodule add -b release-1.8.1 https://github.com/google/googletest googletest
 
 git submodule deinit glog
 git rm --cached glog
-git clone -b huawei/datacom/naas/0.5.0 https://codehub-dg-y.huawei.com/OpenSourceCenter/glog.git
-git submodule add -b huawei/datacom/naas/0.5.0 https://codehub-dg-y.huawei.com/OpenSourceCenter/glog.git glog
+git clone -b huawei/datacom/naas/0.5.0 https://github.com/google/glog.git
+git submodule add -b huawei/datacom/naas/0.5.0 https://github.com/google/glog.git glog
 
 git submodule deinit securec
 git rm --cached securec
-git clone -b tag_Huawei_Secure_C_V100R001C01SPC011B003_00001 https://codehub-dg-y.huawei.com/hwsecurec_group/huawei_secure_c.git securec
-git submodule add -b tag_Huawei_Secure_C_V100R001C01SPC011B003_00001 https://codehub-dg-y.huawei.com/hwsecurec_group/huawei_secure_c.git securec
+git clone -b tag_Huawei_Secure_C_V100R001C01SPC011B003_00001 https://gitee.com/Janisa/huawei_secure_c.git securec
+git submodule add -b tag_Huawei_Secure_C_V100R001C01SPC011B003_00001 https://gitee.com/Janisa/huawei_secure_c.git securec
