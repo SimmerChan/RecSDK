1d692fd5b82eefe5bb46530ef4f9c59119310744
tanfeng <823018000@qq.com>
2025-07-01 19:04:57 +0800

    区分st,dt.流水线适配

--- torchrec/hybrid_torchrec/hybrid_torchrec/__init__.py
+++ torchrec/hybrid_torchrec/hybrid_torchrec/__init__.py
@@ -6,10 +6,12 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import os
 import logging
+import os
 import sysconfig
+
 import torch
+
 from hybrid_torchrec.modules.hash_embeddingbag import (
     HashEmbeddingBagCollection,
     HashEmbeddingBagConfig,
