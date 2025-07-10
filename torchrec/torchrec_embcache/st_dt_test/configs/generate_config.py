1bd33d9141749bc41ebd066413b0af1672395ac2
wuyangjian <wuyangjian1115@163.com>
2025-07-10 14:30:31 +0800

    st dt config冲突修复

--- torchrec/torchrec_embcache/st_dt_test/configs/generate_config.py
+++ torchrec/torchrec_embcache/st_dt_test/configs/generate_config.py
@@ -33,8 +33,7 @@ def generate_num_embeddings(num_embeddings_message):
 
 folder_path = sys.argv[1]
 with open(os.path.join(folder_path, "test_cases.jsonl")) as f:
-    for json_line in json.load(f):
-        row = json.loads(json_line)
+    for row in json.load(f):
         test_case_name = row["test_case_name"]
         world_size = row["world_size"]
         table_num = row["table_num"]
