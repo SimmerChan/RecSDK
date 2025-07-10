84bd57e00189f891cdced97ca15ad7a5dd111696
wuyangjian <wuyangjian1115@163.com>
2025-07-10 11:00:22 +0800

    修改jsonl的格式

--- torchrec/torchrec_embcache/st_dt_test/configs/generate_config.py
+++ torchrec/torchrec_embcache/st_dt_test/configs/generate_config.py
@@ -32,7 +32,7 @@ def generate_num_embeddings(num_embeddings_message):
 
 
 folder_path = sys.argv[1]
-with open(os.path.join(folder_path, "test_cases.jsonl")) as f:
+with open(os.path.join(folder_path, "test_cases.json")) as f:
     for json_line in f.readlines():
         row = json.loads(json_line)
         test_case_name = row["test_case_name"]
