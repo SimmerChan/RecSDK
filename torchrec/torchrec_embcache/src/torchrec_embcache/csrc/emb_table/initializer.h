9f190f4040e2b0edd6bc4f7e7e0e62ec6d25b77c
whf <1660398197@qq.com>
2025-07-25 15:21:33 +0800

    param error

--- torchrec/torchrec_embcache/src/torchrec_embcache/csrc/emb_table/initializer.h
+++ torchrec/torchrec_embcache/src/torchrec_embcache/csrc/emb_table/initializer.h
@@ -16,6 +16,7 @@
 #include <algorithm>
 #include <vector>
 
+#include "securec.h"
 #include "common/common.h"
 
 using RandomVPool = std::vector<std::vector<float>>;
@@ -107,7 +108,7 @@ public:
         std::uniform_int_distribution<int> uDistribution(0, cfg.initializerRadomPoolSize - 1);
         int randIndex = uDistribution(engine);
         auto ret = memcpy_s(embeddingAddr, cfg.embDim * sizeof(float), staticPool[randIndex].data(), cfg.embDim * sizeof(float));
-        if (ret != EOK) {
+        if (ret != 0) {
             throw std::runtime_error("memset_s failed when init optimizer data.");
         }
     }
