17d4eb0aca7f155bdd78d6af50a1537c00d1a8a6
zxorange_321 <zxorange321@163.com>
2025-07-31 16:55:45 +0800

    1

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/hstu_dense/test_hstu_performance/test_msprof_npu_hstu.py
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/hstu_dense/test_hstu_performance/test_msprof_npu_hstu.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     
     _, params = read_and_validate_parameters(args.index)
     cmd = f'rm profnpu/ -rf ; msprof --application=\"python3 test_npu_hstu.py --index={args.index}\" --output=profnpu'
-    subprocess.run(cmd, check=True)
+    subprocess.run(cmd.split(' '), check=True)
     
     search_dir = os.path.join(config.NFS_DIR, 'profnpu')
     csv_files = glob.glob(f'{search_dir}/PROF_*/mindstudio_profiler_output/op_stati*.csv')
