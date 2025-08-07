24762eb892c17300a2081a2e25c4aea959bc01c3
rome_zhouyang <zhouyang153@huawei.com>
2025-08-07 16:19:37 +0800

    add common_python 1th

---
+++ training/common/src/pybind/common_binding.cpp
@@ -0,0 +1,54 @@
+//
+// Created by ROME on 2025/8/6.
+//
+#include <dsmi_common_interface.h>
+#include "pybind11/cast.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
+
+namespace py = pybind11;
+namespace
+{
+    int32_t GetLogicID(uint32_t phyid)
+    {
+        uint32_t logicId;
+        int32_t ret = dsmi_get_logicid_from_phyid(phyid, &logicId);
+        if (ret != 0) {
+            return ret;
+        }
+        return logicId;
+    }
+
+    uint32_t GetDeviceCount()
+    {
+        uint32_t count;
+        aclError ec = aclrtGetDeviceCount(&count);
+        if (ec != 0) {
+            throw std::runtime_error("The failed to get device count.");
+        }
+        return count;
+    }
+
+    string GetChipName(uint32_t devID)
+    {
+        int ret = 0;
+        struct dsmi_chip_info_stru info = {{ 0 },
+                                           { 0 },
+                                           { 0 }};
+        ret = dsmi_get_chip_info(devID, &info);
+        if (ret == 0) {
+            stringstream ss;
+            ss << info.chip_name;
+            return ss.str();
+        }
+
+        throw std::runtime_error("dsmi_get_chip_info failed, ret = " + to_string(ret));
+    }
+
+    PYBIND11_MODULE(common_binding, m)
+    {
+        m.def("get_logic_id", &GetLogicID, py::arg("physic_id"));
+        m.def("get_device_count", &GetDeviceCount);
+        m.def("get_chip_name", &GetChipName, py::arg("device_id"));
+    }
+}
