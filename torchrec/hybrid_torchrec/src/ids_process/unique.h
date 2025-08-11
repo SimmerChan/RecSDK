40b52fb0cf86fa8805aeec4e1b932cf2d5340c6f
Taoqiuyu <1042656162@qq.com>
2025-08-11 10:40:36 +0800

    增加版权信息

--- torchrec/hybrid_torchrec/src/ids_process/unique.h
+++ torchrec/hybrid_torchrec/src/ids_process/unique.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * Copyright (c) Huawei Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 #ifndef HYBRID_UNIQUE_H
 #define HYBRID_UNIQUE_H
 #include <omp.h>
