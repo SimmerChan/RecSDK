2c1980cdc9d37f679943d0019d13fdd630cc96f4
徐岩 <rocky.xu@huawei.com>
2025-08-21 15:23:44 +0800

    新增自动求导用例

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/dense_to_jagged/dense_to_jagged.cpp
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/dense_to_jagged/dense_to_jagged.cpp
@@ -123,7 +123,7 @@ public:
 };
 
 // 自动求导接口
-std::tuple<at::Tensor, tensor_list> dense_to_jagged_autograd(const at::Tensor& dense,
+at::Tensor dense_to_jagged_autograd(const at::Tensor& dense,
                                                              const tensor_list& offsets,
                                                              const c10::optional<int64_t> total_L)
 {
