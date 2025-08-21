0ca92e3704352f8d50defb75808d6909bc5dfe68
zxorange_321 <zxorange321@163.com>
2025-08-21 19:57:06 +0800

    1

--- torchrec/torchrec_embcache/src/torchrec_embcache/distributed/sharding/rw_sharding.py
+++ torchrec/torchrec_embcache/src/torchrec_embcache/distributed/sharding/rw_sharding.py
@@ -89,7 +89,6 @@ class EmbCacheRwSparseFeaturesDist(RwSparseFeaturesDist):
         need_pos: bool = False,
         keep_original_indices: bool = False,
         enable_admit: bool = False,
-        is_ec: bool = False,
     ) -> None:
         super().__init__(
             pg,
@@ -106,7 +105,8 @@ class EmbCacheRwSparseFeaturesDist(RwSparseFeaturesDist):
 
         # local unique只可用于EC(Embedding Collection / Sequence Embedding)
         yes_str = ("true", "1", "yes")
-        self._do_unique = os.environ.get("DO_EC_LOCAL_UNIQUE", "False").lower() in yes_str and is_ec
+        self._do_unique = os.environ.get("DO_EC_LOCAL_UNIQUE", "False").lower() in yes_str and \
+                          os.environ.get("USE_EC", "False").lower() in yes_str
 
         self._enable_admit = enable_admit
 
--- torchrec/torchrec_embcache/src/torchrec_embcache/sparse/__init__.py
+++ torchrec/torchrec_embcache/src/torchrec_embcache/sparse/__init__.py
@@ -4,20 +4,15 @@
 # All rights reserved.
 #
 # This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
 
-from hybrid_torchrec.sparse.jagged_tensor_with_count import (
-    JaggedTensorWithCount,
-    KeyedJaggedTensorWithCount,
-)
+# LICENSE file in the root directory of this source tree.
 from torchrec_embcache.sparse.jagged_tensor_with_timestamp import (
     JaggedTensorWithTimestamp,
     KeyedJaggedTensorWithTimestamp,
 )
 
+
 __all__ = [
-    JaggedTensorWithCount,
-    KeyedJaggedTensorWithCount,
     JaggedTensorWithTimestamp,
     KeyedJaggedTensorWithTimestamp,
 ]
