ac2658eb1a1f53efb2b4ed901c0687aa2074b6a4
zxorange_321 <zxorange321@163.com>
2025-08-22 11:02:52 +0800

    Fix import order in test_feature_filter.py

--- torchrec/torchrec_embcache/tests/acc_test/test_feature_filter.py
+++ torchrec/torchrec_embcache/tests/acc_test/test_feature_filter.py
@@ -21,12 +21,6 @@ import torch.distributed as dist
 from torch import nn, Tensor
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.data import DataLoader
-from torchrec_embcache.distributed.embedding import EmbCacheEmbeddingCollection
-from torchrec_embcache.distributed.configs import (EmbCacheEmbeddingConfig,
-                                                   AdmitAndEvictConfig)
-from torchrec_embcache.distributed.train_pipeline import EmbCacheTrainPipelineSparseDist
-from torchrec_embcache.distributed.sharding.embedding_sharder import EmbCacheEmbeddingCollectionSharder
-from torchrec_embcache.sparse.jagged_tensor_with_timestamp import KeyedJaggedTensorWithTimestamp
 import torchrec
 import torchrec.distributed
 from torchrec import EmbeddingCollection
@@ -39,7 +33,13 @@ from torchrec.distributed.planner import (
 from torchrec.distributed.types import ShardingEnv
 from torchrec.optim.keyed import CombinedOptimizer
 
-# 本地导入应该放在第三方库导入之后
+from torchrec_embcache.distributed.embedding import EmbCacheEmbeddingCollection
+from torchrec_embcache.distributed.configs import (EmbCacheEmbeddingConfig,
+                                                   AdmitAndEvictConfig)
+from torchrec_embcache.distributed.train_pipeline import EmbCacheTrainPipelineSparseDist
+from torchrec_embcache.distributed.sharding.embedding_sharder import EmbCacheEmbeddingCollectionSharder
+from torchrec_embcache.sparse.jagged_tensor_with_timestamp import KeyedJaggedTensorWithTimestamp
+
 from dataset import RandomRecDataset, Batch
 from model import ModelEc as Model
 from util import setup_logging
