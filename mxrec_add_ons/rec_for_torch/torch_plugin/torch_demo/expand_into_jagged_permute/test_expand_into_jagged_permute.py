7ffaf32026a4f922c4fe054d244acc60c63f07a5
徐岩 <rocky.xu@huawei.com>
2025-08-22 17:10:45 +0800

    新增自动求导用例

---
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/expand_into_jagged_permute/test_expand_into_jagged_permute.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+import itertools
+import logging
+import sysconfig
+
+import pytest
+import fbgemm_gpu
+import numpy as np
+import torch_npu
+import torch
+
+DEVICE = "npu:0"
+logging.getLogger().setLevel(logging.INFO)
+torch.ops.load_library(f"{sysconfig.get_path('purelib')}/libfbgemm_npu_api.so")
