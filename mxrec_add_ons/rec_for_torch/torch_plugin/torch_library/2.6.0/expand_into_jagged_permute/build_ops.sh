26580c9790eff5750dd6cd96214239b951a76301
徐岩 <rocky.xu@huawei.com>
2025-08-25 11:51:11 +0800

    新增自动求导用例

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/expand_into_jagged_permute/build_ops.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-rm -rf build
-mkdir -p build
-cmake -B build
-cmake --build build -j
-chmod 550 ./build/*.so
-export LD_LIBRARY_PATH=$ASCEND_OPP_PATH/vendors/expand_into_jagged_permute/op_api/lib:$LD_LIBRARY_PATH
