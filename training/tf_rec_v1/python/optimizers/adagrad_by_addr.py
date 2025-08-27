b9cc0e3e5b93be0c3258df07a4b5982bbe4c9020
lmdgks <zhouyang153@huawei.com>
2025-08-27 09:46:05 +0800

    split tests

--- training/tf_rec_v1/python/optimizers/adagrad_by_addr.py
+++ training/tf_rec_v1/python/optimizers/adagrad_by_addr.py
@@ -24,15 +24,15 @@ from tensorflow.python.ops import math_ops
 from tensorflow.python.training import adagrad
 from tensorflow.python.training.optimizer import Optimizer
 
-from mx_rec.optimizers.base import CustomizedOptimizer
-from mx_rec.util.initialize import ConfigInitializer
-from mx_rec.util.ops import import_host_pipeline_ops
-from mx_rec.validator.validator import (
+from rec_sdk_common.validator.validator import (
     FloatValidator,
     StringValidator,
     para_checker_decorator,
     LearningRateValidator,
 )
+from mx_rec.optimizers.base import CustomizedOptimizer
+from mx_rec.util.initialize import ConfigInitializer
+from mx_rec.util.ops import import_host_pipeline_ops
 
 
 @para_checker_decorator(
