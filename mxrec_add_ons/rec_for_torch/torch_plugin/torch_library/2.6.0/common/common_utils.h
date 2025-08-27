ed5ffe5efdce4888a0ebe2bc06088d361053a51c
xyllq999 <734432375@qq.com>
2025-08-27 19:17:18 +0800

    1

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/common/common_utils.h
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/common/common_utils.h
@@ -1,3 +1,13 @@
+/**
+ * @file common_utils.h
+ *
+ * Copyright (C) 2025. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 #ifndef COMMON_UTILS_H
 #define COMMON_UTILS_H
 
