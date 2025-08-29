0156e9aabbfec9a44e933f268d03f018e3d7ae9c
lmdgks <zhouyang153@huawei.com>
2025-08-29 10:55:22 +0800

    modify test_ut

--- training/common/src/test_ut.sh
+++ training/common/src/test_ut.sh
@@ -141,10 +141,10 @@ COVERAGE_FILE=coverage.info
 REPORT_FOLDER=coverage_report
 lcov --rc lcov_branch_coverage=1 --filter branch -c -d build -o "${COVERAGE_FILE}"_tmp
 lcov -r "${COVERAGE_FILE}"_tmp 'ut/*' '7/ext*' '*7/bits*' 'platform/*' '/usr/local/*' '/usr/include/*' '/opt/buildtools/python-3.7.5/lib/python3.7/site-packages/tensorflow*' '/opt/rh/devtoolset-7/root/usr/lib/gcc/x86_64-redhat-linux/7/include/*' 'tests/*' --rc lcov_branch_coverage=1 --filter branch --ignore-errors unused,unused -o "${COVERAGE_FILE}"
-genhtml "${COVERAGE_FILE}" --output-directory "${REPORT_FOLDER}" --branch-coverage --filter branch
-[ -d "${COVERAGE_FILE}"_tmp ] && rm -rf "${COVERAGE_FILE}"_tmp
-[ -d "${COVERAGE_FILE}" ] && rm -rf "${COVERAGE_FILE}"
+#genhtml "${COVERAGE_FILE}" --output-directory "${REPORT_FOLDER}" --branch-coverage --filter branch
+#[ -d "${COVERAGE_FILE}"_tmp ] && rm -rf "${COVERAGE_FILE}"_tmp
+#[ -d "${COVERAGE_FILE}" ] && rm -rf "${COVERAGE_FILE}"
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    open ./"${REPORT_FOLDER}"/index.html
-fi
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+#    open ./"${REPORT_FOLDER}"/index.html
+#fi
