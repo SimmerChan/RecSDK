f25ccab21a57077230bc8a3ebb914643fce31b5c
xiajunhua <xiajunhua1@huawei.com>
2025-09-01 14:41:42 +0800

    modify hstu bf16 type precision

--- examples/ops/torch/hstu_dense_backward_fuxi/test_hstu_dense_backward_fuxi.py
+++ examples/ops/torch/hstu_dense_backward_fuxi/test_hstu_dense_backward_fuxi.py
@@ -30,7 +30,7 @@ from utils import allclose
 
 torch.npu.config.allow_internal_format = False
 CURR_DIR = Path(__file__).resolve().parent
-torch.ops.load_library(str(CURR_DIR.parent.parent.parent.parent / 	
+torch.ops.load_library(str(CURR_DIR.parent.parent.parent.parent / 
                            "cust_op/framework/torch_plugin/torch_library/2.6.0/hstu_dense_backward_fuxi/build/libhstu_dense_fuxi_backward_ops.so"))
 
 device_id: int = 0
