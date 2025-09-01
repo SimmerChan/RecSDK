3ca4350af1842d03c42df3883c297814e7c8a643
penghuiyang <1060916628@qq.com>
2025-09-01 20:07:13 +0800

    保存加载路径检查修改&加载优化

--- torchrec/torchrec_embcache/src/torchrec_embcache/csrc/embedding_cache/embcache_manager.h
+++ torchrec/torchrec_embcache/src/torchrec_embcache/csrc/embedding_cache/embcache_manager.h
@@ -200,7 +200,7 @@ private:
     void LoadFeatureAdmitAndEvictInfo(const std::shared_ptr<FileSystem>& fileSystemPtr,
                                       int32_t tableIndex, const std::string& filePrefix,
                                       const std::vector<int64_t>& saveKeys);
-    int32_t GetOneTimeLoadCount(int32_t embDim);
+    static int32_t GetOneTimeLoadCount(int32_t embDim);
 private:
     int32_t embNum_;
     std::vector<int32_t> embTableIndies_;
