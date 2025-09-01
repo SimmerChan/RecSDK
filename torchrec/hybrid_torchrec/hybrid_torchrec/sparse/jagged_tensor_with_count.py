354051ff3877b662dd760f0546f027c91b92f13b
zxorange_321 <zxorange321@163.com>
2025-09-01 09:59:17 +0800

    1

--- torchrec/hybrid_torchrec/hybrid_torchrec/sparse/jagged_tensor_with_count.py
+++ torchrec/hybrid_torchrec/hybrid_torchrec/sparse/jagged_tensor_with_count.py
@@ -1,3 +1,35 @@
+#!/usr/bin/env python3
+# Copyright (c) Huawei Technologies Co., Ltd. 2024. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+from typing import Optional, Dict, List, Tuple
+
+import torch
+
+from torch.autograd.profiler import record_function
+from torchrec.sparse.jagged_tensor import (
+    _permute_tensor_by_segments,
+    _sum_by_splits,
+    JaggedTensor,
+    KeyedJaggedTensor,
+)
+from torchrec.pt2.checks import is_non_strict_exporting
+
+from .extended_jagged_tensor import ExtendedJaggedTensor, KeyedExtendedJaggedTensor
+
+
 class KeyedJaggedTensorWithCount(KeyedExtendedJaggedTensor):
     _fields = [
         "_counts"
