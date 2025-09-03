dfbaf4e9330447f9f36e2878744c9256d9989f59
zhouyubuzhaozhong <haoripei@huawei.com>
2025-09-03 11:44:45 +0800

    refactor torchrec examples and docker files

--- torch_examples/little_demo/model.py
+++ torch_examples/little_demo/model.py
@@ -19,6 +19,10 @@ import torch
 from torchrec import PoolingType
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 49efaece (refactor torchrec examples and docker files)
 class TestModel(torch.nn.Module):
     def __init__(self, table_names, feat_names, embed_dims, num_embeds):
         super().__init__()
