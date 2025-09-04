a5ca691d52f063252cef1fc9988432ba9a891855
penghuiyang <1060916628@qq.com>
2025-09-04 16:42:41 +0800

    [FIX]多进程保存时时间戳名称同步

--- training/torch_rec_v1/torchrec_embcache/src/torchrec_embcache/saver.py
+++ training/torch_rec_v1/torchrec_embcache/src/torchrec_embcache/saver.py
@@ -6,6 +6,7 @@
 # LICENSE file in the root directory of this source tree.
 import logging
 import os
+import time
 from datetime import datetime, timezone
 from pathlib import Path
 
@@ -86,6 +87,7 @@ class Saver:
         self._check_emb_cache_instance_len()
         if not os.path.exists(path):
             os.makedirs(path, _DIR_MODE, exist_ok=True)
+        time.sleep(1)  # avoid the C++ size unable to recognize the created folder.
         logging.info("In save scene, path:%s, cache_module info:%s", path, self.cache_module)
         for mod in self.cache_module:
             logging.info("In save scene, embcache_mgr info:%s", mod.embcache_mgr)
