88d6c09b9dc7ad657f165682adf496e23d36a440
whf <1660398197@qq.com>
2025-09-05 16:06:20 +0800

    bug fix

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/hstu_dense/test_hstu_dense_forward_demo.py
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/hstu_dense/test_hstu_dense_forward_demo.py
@@ -48,7 +48,7 @@ def skip_seq_len(seq_len):
     return False
 
 
-def jagged_data_gen(batch_size, max_seq_len, num_heads, attention_dim, data_type, mask_type, num_context, num_target, target_group_size):
+def jagged_data_gen(batch_size, max_seq_len, num_heads, attention_dim, data_type, mask_type, num_context=None, num_target=None, target_group_size=None):
     seq_lens = np.random.randint(1, max_seq_len + 1, (batch_size))
 
     seq_offset = torch.concat((torch.zeros((1,), dtype=torch.int64), \
@@ -70,19 +70,18 @@ def jagged_data_gen(batch_size, max_seq_len, num_heads, attention_dim, data_type
         rel_attn_bias[batch_id, :, 0:seq_len, 0:seq_len] = torch.rand(seq_len, seq_len).to(torch.float32)
 
     if mask_type == mask_tril:
-        # invalid_attn_mask = 1 - torch.triu(torch.ones(batch_size, num_heads, max_seq_len, max_seq_len), diagonal=1)
-        batched_mask = []
-        invalid_attn_mask = torch.zeros(num_heads, max_seq_len, max_seq_len)
+        invalid_attn_mask = torch.zeros(batch_size, num_heads, max_seq_len, max_seq_len)
         for sample_id, seq_len in enumerate(seq_lens):
             parm = ScoreShapeParam(
                 seq_len=seq_len,
                 num_target=num_target,
                 num_context=num_context,
                 num_history=None if num_target is None else seq_len-num_target,
+                target_group_size=target_group_size,
                 block_h=MASK_BLOCK_SIZE,
                 block_w=MASK_BLOCK_SIZE
             )
-            mask_tensor = compute_target_mask_each_block_concat(parm)
+            mask_tensor = compute_target_mask_each_block_concat(parm, use_npu=False)
             invalid_attn_mask[sample_id, :, :seq_len, :seq_len] = mask_tensor
             
         
@@ -204,7 +203,7 @@ class TestHstuJaggedDemo:
         torch.npu.synchronize()
         return atten_output.to(data_type).reshape(-1)
 
-    def execute(self, batch_size, max_seq_len, head_num, head_dim, enable_bias, mask_type, silu_scale, data_type, num_context, num_target, target_group_size):
+    def execute(self, batch_size, max_seq_len, head_num, head_dim, enable_bias, mask_type, silu_scale, data_type, num_context=None, num_target=None, target_group_size=None):
         q, k, v, seq_offset, bias, mask, max_seq_len = jagged_data_gen(batch_size, max_seq_len, head_num, head_dim,
                                                                        data_type, mask_type, num_context, num_target, target_group_size)
 
