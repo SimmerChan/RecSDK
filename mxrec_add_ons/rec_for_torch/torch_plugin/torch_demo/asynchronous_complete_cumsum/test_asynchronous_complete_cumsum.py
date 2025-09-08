35b111eec3c43370d70e24c2b16cc6730aa04b0f
zxorange_321 <zxorange321@163.com>
2025-09-08 16:46:55 +0800

    1

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/asynchronous_complete_cumsum/test_asynchronous_complete_cumsum.py
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_demo/asynchronous_complete_cumsum/test_asynchronous_complete_cumsum.py
@@ -42,6 +42,7 @@ def test_asynchronous_complete_cumsum(dtype, device, length):
     result = get_ops_result(t_int.to(device))
     assert torch.allclose(result, golden)
 
+
 @pytest.mark.parametrize("length", [2**31 - 1, 2**31])
 def test_asynchronous_complete_cumsum_exceed_limit(exceed_length):
     """测试超过元素数量限制的情况，验证PTA层校验机制"""
