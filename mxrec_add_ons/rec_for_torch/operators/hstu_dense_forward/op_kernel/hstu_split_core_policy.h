18490143005aa0aaee28973d7d97baff2440c019
tqy1998 <taoqiuyu2@huawei.com>
2025-09-08 19:24:01 +0800

    copyright fix

--- mxrec_add_ons/rec_for_torch/operators/hstu_dense_forward/op_kernel/hstu_split_core_policy.h
+++ mxrec_add_ons/rec_for_torch/operators/hstu_dense_forward/op_kernel/hstu_split_core_policy.h
@@ -1,4 +1,4 @@
-/* Copyright 2025. Huawei Technologies Co.,Ltd. All rights reserved.
+/* Copyright (c) Huawei Technologies Co.,Ltd. 2025. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
