b572332c28e6b5e7158190cf7fca2489182ce900
zxorange_321 <zxorange321@163.com>
2025-09-08 14:42:29 +0800

    1

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/asynchronous_complete_cumsum/asynchronous_complete_cumsum.cpp
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/asynchronous_complete_cumsum/asynchronous_complete_cumsum.cpp
@@ -21,7 +21,6 @@ at::Tensor asynchronous_complete_cumsum_npu(const at::Tensor &offset)
     TORCH_CHECK(offset.dtype() == at::kInt || offset.dtype() == at::kLong,
                 "asynchronous_complete_cumsum: Offset tensor must be of type int32 or int64");
     TORCH_CHECK(offset.dim() == 1, "asynchronous_complete_cumsum: Offset tensor must be 1-D");
-
     TORCH_CHECK(offset.numel() < std::numeric_limits<int32_t>::max(),
                 "asynchronous_complete_cumsum: Offset tensor must have less than INT_MAX elements, but got ",
                 offset.numel());
